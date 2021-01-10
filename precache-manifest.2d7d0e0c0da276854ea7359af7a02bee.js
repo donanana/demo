9431764b62deaa180f137268fe243c71c1b5daaa
donanana <sunghajung28i39@gmail.com>
2021-01-10 17:59:36 +0800

    Updates

---
+++ precache-manifest.2d7d0e0c0da276854ea7359af7a02bee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4161e07c1c96725bbd9ad6d47f84d83b",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "f873ca45bedc6ba4ca07",
+    "url": "/demo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e6e82c0c83c900b3d337",
+    "url": "/demo/static/css/main.afd1b6ce.chunk.css"
+  },
+  {
+    "revision": "f873ca45bedc6ba4ca07",
+    "url": "/demo/static/js/2.a18b5c7c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/demo/static/js/2.a18b5c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e82c0c83c900b3d337",
+    "url": "/demo/static/js/main.a8950356.chunk.js"
+  },
+  {
+    "revision": "40d46c5e050f9368d9b8",
+    "url": "/demo/static/js/runtime-main.8814667a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/donanana/demo.git/precache-manifest.250202f98217835fd8088f7b5b2cc678.js"
+  "/demo/precache-manifest.2d7d0e0c0da276854ea7359af7a02bee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/donanana/demo.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
